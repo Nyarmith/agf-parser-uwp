9cd6787eb3f946076ee7bc56d6f0327760d76719
Nyarmith <powah.serge@gmail.com>
2018-02-28 22:29:29 -0500

    added more structure

---
+++ agf-parser-uwp/Parser/ActiveGame.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace agf_parser_uwp.Parser
+{
+    //essentially the AdventureGame class from the python version
+    class ActiveGame
+    {
+        private AdventureGame game; //adventure game data structure
+        private string state;       //ptr to current state
+        
+
+        public string[] getChoices() {
+            string[] ret = new string[] { "temp" };
+            return ret;
+        }
+
+        public string getText() {
+            return "temp";
+        }
+    }
+}
