6a1ec53dff419344b34c6355084888a9521b40c8
Nyarmith <powah.serge@gmail.com>
2018-04-18 21:51:22 -0400

    creator

--- agf-parser-uwp/Pages/Creator.xaml.cs
+++ agf-parser-uwp/Pages/Creator.xaml.cs
@@ -22,6 +22,33 @@ namespace agf_parser_uwp
     /// </summary>
     public sealed partial class Creator : Page
     {
+        private AdventureGame ag;
+
+        public class StateSelector : DataTemplateSelector
+        {
+            public DataTemplate DefaultTemplate { get; set; }
+            public DataTemplate SelectedItemTemplate { get; set; }
+
+            protected override DataTemplate SelectTemplateCore(Object item, DependencyObject container)
+            {
+                if (container is GridViewItem c)
+                {
+                    if (c.Tag != null && long.TryParse(c.Tag.ToString(), out var tkn))
+                    {
+                        c.UnregisterPropertyChangedCallback(GridViewItem.IsSelectedProperty, tkn);
+                    }
+
+                    c.Tag = c.RegisterPropertyChangedCallback(GridViewItem.IsSelectedProperty, (s, e) =>
+                    { c.ContentTemplateSelector = null; c.ContentTemplateSelector = this; });
+
+                    if (c.IsSelected)
+                        return SelectedItemTemplate;
+
+                }
+                return DefaultTemplate;
+            }
+        }
+
         public Creator()
         {
             this.InitializeComponent();
